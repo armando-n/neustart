38309ae3e2d38847ba6cb9414335d9ae2ce1a02f
Armando Navarro <armando.n@outlook.com>
2018-02-11 23:13:11 -0800

    setup composer, index, includer, twilio, htacecss, gitignore

---
+++ includer.php
@@ -0,0 +1,22 @@
+<?php
+$pathDir = dirname(__FILE__);
+$paths = array("views", "models", "controllers", "resources", "exceptions");
+$includePath = get_include_path();
+foreach ($paths as $path) {
+	$includePath .= PATH_SEPARATOR . $pathDir . DIRECTORY_SEPARATOR . $path;
+}
+set_include_path($includePath);
+
+spl_autoload_register('myClassLoader');
+
+function myClassLoader($className) {
+	$paths = explode(PATH_SEPARATOR, get_include_path());
+	foreach ($paths as $path) {
+		$file = $path . DIRECTORY_SEPARATOR . $className . '.class.php';
+		if (file_exists($file)) {
+			include_once $file;
+			break;
+		}
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+try {
+// if (ob_get_contents() === false)
+//     ob_start();
+include_once("includer.php");
+if (!isset($_SESSION)) {
+    session_start();
+    $_SESSION['styles'] = array();
+    $_SESSION['scripts'] = array();
+    $_SESSION['libraries'] = array();
+    $_SESSION['dbName'] = 'neustart';
+    $_SESSION['configFile'] = 'myConfig.ini';
+}
+
+$_SESSION['base'] = '/';
+
+// parse the request URL
+$url = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH); // e.g. /dhma/home or /dhma/signup_show
+$urlParts = preg_split("/\//", $url, null, PREG_SPLIT_NO_EMPTY);
+$urlPartsLength = count($urlParts);
+
+// determine control, if any
+if ($urlPartsLength > 1) {
+	$control = $urlParts[0];
+	if ( ($hashPos = strrpos($control, '#')) !== false)
+		$control = substr($control, 0, $hashPos);
+	$_SESSION['control'] = $control;
+} else {
+	$control = "none";
+	unset($_SESSION['control']);
+}
+
+// $_SESSION['base'] = $urlParts[0];
+
+// determine action, if any
+if ($urlPartsLength > 1) {
+	$action = $urlParts[1];
+	$_SESSION['action'] = $action;
+} else
+	unset($_SESSION['control']);
+
+// process any arguments
+$arguments = array();
+if ($urlPartsLength > 2) {
+	for ($i = 2; $i < $urlPartsLength; $i++)
+		$arguments[] = $controlParts[$i];
+	$_SESSION['arguments'] = $arguments;
+} else {
+	unset($_SESSION['arguments']);
+}
+
+// run the requested controller
+switch ($control) {
+    case "home":
+    default:
+        $_SESSION['styles'] = array('HomeStyles.css');
+        HomeView::show();
+}
+
+//ob_end_flush();
+} catch (Exception $e) {
+    $_SESSION['flash'] = 'Unable to complete request: An unexpected error occured.';
+    HomeView::show();
+}
+?>
