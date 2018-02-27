b2c836582a5616d427a145db7fbaaccb640d3bdc
Armando Navarro <armando.n@outlook.com>
2018-02-27 04:12:36 -0800

    Babel transpiler, Webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+	entry: './es6/Dashboard.js',
+	output: {
+		filename: 'js/Dashboard.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['../node_modules/babel-preset-env']
+					}
+				}
+			}
+		]
+	},
+	resolve: {
+		alias: {
+			modules: './node_modules/'
+		}
+	}
+};
